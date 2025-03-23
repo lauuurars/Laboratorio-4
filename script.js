b990a4b45d79011a04aa9c9e598fbecd0669b1cf
lauuurars <1120098177@u.icesi.edu.co>
2025-03-22 21:16:22 -0500

    se realizó código javascript

---
+++ script.js
@@ -0,0 +1,32 @@
+
+document.getElementById("calcular").addEventListener("click", function() {
+    // nombrar las variables
+
+    let op1 = document.getElementById("operador1").value;
+    let operador = document.getElementById("operadores").value;
+    let op2 = document.getElementById("operador2").value;
+    let resultado = document.getElementById("resultado");
+    let num1 = parseInt(op1);
+    let num2 = parseInt(op2);
+
+    if (isNaN(op1) || isNaN(op2)) {
+        resultado.textContent = "ERROR: Ingresa números válidos.";
+    }
+
+    // apartado de las operaciones
+    if (operador === "+") {
+        resultado.textContent = "Resultado: " + (num1 + num2);
+    } else if (operador === "-") {
+        resultado.textContent = "Resultado: " + (num1 - num2);
+    }else if (operador === "*") {
+        resultado.textContent = "Resultado: " + (num1 * num2);
+    } else if (operador === "/") {
+        if (num2 === 0) {
+            resultado.textContent = "ERROR: ¡No se puede dividir entre cero!";
+        } else {
+            resultado.textContent = "Resultado: " + (num1 / num2);
+        }
+    } else {
+        resultado.textContent = "ERROR: ¡Operador No Válido!";
+    }
+});
